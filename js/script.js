d086035429f65b8c77391598b1f8338e872e8eaa
Vikiez <121822471+Vikiez@users.noreply.github.com>
2026-05-14 20:15:31 +0300

    Загрузил все папки проекта

---
+++ js/script.js
@@ -0,0 +1,26 @@
+console.log(123);
+
+$(document).ready(function() {
+  $('.header-top__btn').click(function(event) {
+    $('.header-top__btn,.header-top__menu-big').toggleClass('active');
+    // $('body').toggleClass('lock');
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
